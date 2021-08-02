f28273b7d38d91124069d34814e9c1e97184916b
Andy Footner <andyfootner@netscape.net>
2021-08-02 18:29:01 +0200

    [POCAM-62] update to D9 without conflicts

--- scripts/composer/ScriptHandler.php
+++ scripts/composer/ScriptHandler.php
@@ -11,7 +11,6 @@ use Composer\Script\Event;
 use Composer\Semver\Comparator;
 use DrupalFinder\DrupalFinder;
 use Symfony\Component\Filesystem\Filesystem;
-use Webmozart\PathUtil\Path;
 
 /**
  * Class ScriptHandler.
@@ -39,22 +38,6 @@ class ScriptHandler {
         $fs->mkdir($drupalRoot . '/' . $dir);
         $fs->touch($drupalRoot . '/' . $dir . '/.gitkeep');
       }
-    }
-
-    // Prepare the settings file for installation.
-    if (!$fs->exists($drupalRoot . '/sites/default/settings.php') and $fs->exists($drupalRoot . '/sites/default/default.settings.php')) {
-      $fs->copy($drupalRoot . '/sites/default/default.settings.php', $drupalRoot . '/sites/default/settings.php');
-      require_once $drupalRoot . '/core/includes/bootstrap.inc';
-      require_once $drupalRoot . '/core/includes/install.inc';
-      $settings['config_directories'] = [
-        CONFIG_SYNC_DIRECTORY => (object) [
-          'value' => Path::makeRelative($drupalFinder->getComposerRoot() . '/config/sync', $drupalRoot),
-          'required' => TRUE,
-        ],
-      ];
-      drupal_rewrite_settings($settings, $drupalRoot . '/sites/default/settings.php');
-      $fs->chmod($drupalRoot . '/sites/default/settings.php', 0666);
-      $event->getIO()->write("Create a sites/default/settings.php file with chmod 0666");
     }
 
     // Create the files directory with chmod 0777.
